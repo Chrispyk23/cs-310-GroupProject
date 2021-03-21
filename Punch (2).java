ae706aa2dfd1ce7e9e36d44fb5a7ca247f0312cd
Chris <cknox1@stu.jsu.edu>
2021-03-21 12:00:31 -0500

    GP

---
+++ Punch (2).java
@@ -0,0 +1,67 @@
+
+package edu.jsu.mcis.cs310.tas_sp21;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Punch {
+  private String adjustmenttype;
+  private int terminalid;
+  private int punchtypeid;
+  private static int id;
+  private long originaltimestamp;
+  Badge badge;
+  private GregorianCalendar gc = new GregorianCalendar ();
+  
+          
+  public Punch(int terminalid, Badge badge, long originaltimestamp, int punchtypeid){
+        this.punchtypeid = punchtypeid;
+        this.terminalid = terminalid;
+        this.originaltimestamp = originaltimestamp;
+        this.badge = badge;
+  }
+   public Punch(int id){
+       Punch.id = id;
+   }
+     public Badge getbadge(){
+     return badge;
+ }
+    public int getid(){
+        return id;
+    }
+   public int getterminalid(){
+      return terminalid;
+  }
+   public long getoriginaltimestamp(){
+        return originaltimestamp; 
+   }
+   public int getpunchtypeid(){
+      return punchtypeid;
+  }
+   public void setOriginalTimeStamp(long ts){
+       gc.setTimeInMillis(originaltimestamp);
+       this.originaltimestamp = ts;
+   }
+    
+    public String printOriginalTimestamp(){
+      StringBuilder sb = new StringBuilder("#");
+      sb.append(badge);
+      switch(punchtypeid){
+          case 0:
+            sb.append(" CLOCKED OUT:");
+            break;
+          case 1:
+            sb.append(" CLOCKED IN:");
+            break; 
+          case 2:
+            sb.append(" TIMED OUT:");
+            break;
+          default:
+            sb.append(" ERROR");
+      }
+       sb.append(originaltimestamp);
+       
+       SimpleDateFormat sdf = new SimpleDateFormat("EEE MM/dd/yyyy HH:mm:ss");
+       sb.append(sdf.format(gc.getTime()).toUpperCase());
+       return sb.toString();
+    }
+}
